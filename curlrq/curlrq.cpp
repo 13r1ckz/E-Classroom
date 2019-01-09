1e3bdcc7dcd3cfc76057ff6569ecf57c5074c82c
13r1ckz2 <main@13r1ckz.com>
2019-01-09 14:30:41 +0100

    add comments to file commnts added for doxygen needs json parsing

--- curlrq/curlrq.cpp
+++ curlrq/curlrq.cpp
@@ -1,30 +1,103 @@
 #include <iostream>
-#include <string.h>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <ctime>
+#include <iomanip>
 #include <curl/curl.h>
-
-
+ 
+void curlRequest(std::string* buffer, int ID);
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
+std::string getCurlURL(int ID);
+std::string getDateField();
+std::string getIdField(int ID);
+ 
+std::string BaseURL = "https://roostertest.windesheim.nl/WebUntis/Timetable.do?request.preventCache=1543327601812&";
+std::string RequestClassData = "ajaxCommand=getWeeklyTimetable&elementType=4&departmentId=0&filterId=2&";
+ 
+ 
+int main(void){
+    std::string readBuffer;
+    curlRequest(&readBuffer, 328);
+    std::cout << readBuffer << std::endl;
+}
+ 
+/**
+* @brief This function requests Room data from roostertest.windesheim.nl by RoomID.
+* @retuns none
+* @note needs a pointer to a string buffer & RoomID int
+*/
+void curlRequest(std::string* buffer, int ID){
+    CURL *curl;
+    CURLcode res;
+    std::string URLString = getCurlURL(ID);
+    //String to Char array
+    char * URL = new char [URLString.length()+1];
+    strcpy (URL, URLString.c_str());
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, URL);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, buffer);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+        curl_easy_setopt(curl, CURLOPT_COOKIE, "schoolname=\"Windesheim\"");
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+}
+ 
+/**
+* @brief This function is used by curlRequest as WriteCallback
+* @retuns
+* @note
+*/
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp){
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
-
-int main(void){
-    CURL *curl;
-    CURLcode res;
-    std::string readBuffer;
-    curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_easy_setopt(curl, CURLOPT_URL, "https://roostertest.windesheim.nl/WebUntis/Timetable.do?request.preventCache=1543327601812&ajaxCommand=getWeeklyTimetable&elementType=4&date=20190109&departmentId=0&filterId=2&elementId=328");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-       // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-       // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-        curl_easy_setopt(curl, CURLOPT_COOKIE, "schoolname=\"Windesheim\"");
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-    }
-    std::cout << res << std::endl;
-    std::cout << readBuffer << std::endl;
-	
+ 
+/**
+* @brief This function creates the curl request URL with Date & RoomID.
+* @retuns String of URL
+* @note Needs a RoomID
+*/
+std::string getCurlURL(int ID){
+    std::string Date = getDateField();
+    std::string RoomID = getIdField(ID);
+    std::string URL = BaseURL + RequestClassData + Date + RoomID;
+    return URL;
+}
+ 
+/**
+* @brief This function creates Date part of the curl request URL.
+* @retuns String with date in following format: data=YYYYMMDD&
+* @note
+*/
+std::string getDateField(){
+    time_t t = time(NULL);
+    tm* timePtr = localtime(&t);
+    std::ostringstream streamDate;
+    streamDate << "date=" << (timePtr->tm_year + 1900) << std::setw(2) << std::setfill('0') << (timePtr->tm_mon + 1) << std::setw(2) << std::setfill('0') << timePtr->tm_mday << "&";
+    std::string Date(streamDate.str());
+    return Date;
+}
+ 
+/**
+* @brief This function creates RoomID part of the curl request URL.
+* @retuns String with RoomID in following format: elementId=RoomID.
+* @note Needs a RoomID
+*/
+std::string getIdField(int ID){
+    std::ostringstream streamID;
+    streamID << "elementId=" << ID;
+    std::string RoomID(streamID.str());
+    return RoomID;
 }
