c602323bc3f67729f2c3a2ed5fc404f3308cf689
13r1ckz2 <main@13r1ckz.com>
2019-01-11 15:41:14 +0100

    Out put format. out put format on richt way only needs to be set to functions and fomated for transmission to node

--- curlrq/curlrq.cpp
+++ curlrq/curlrq.cpp
@@ -12,41 +12,84 @@ void curlRequest(std::string* buffer, int ID);
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
 std::string getCurlURL(int ID);
 std::string getDateField();
+std::string getDate(int days);
 std::string getIdField(int ID);
 
 std::string BaseURL = "https://roostertest.windesheim.nl/WebUntis/Timetable.do?request.preventCache=1543327601812&";
 std::string RequestClassData = "ajaxCommand=getWeeklyTimetable&elementType=4&departmentId=0&filterId=2&";
 
+int ClassID = 328;
 
 int main(void){
-    std::string readBuffer;
-    curlRequest(&readBuffer, 328);
+    	std::string lessonText;
+	std::string startTime;
+	std::string endTime;
+	std::string teachers;
+	std::string date;
+	int lessonId;
+	std::string lessonTime;
 
-    Json::Reader reader;
-    Json::Value obj;
-    reader.parse(readBuffer, obj); // reader can also read strings
-    const Json::Value& characters = obj["result"]["data"]["elementPeriods"]["328"]; // array of characters
-    for (int i = 0; i < characters.size(); i++){
-        std::cout << " " <<std::endl;
-	std::cout << "        Name: " << characters[i]["lessonText"] << std::endl;
-        std::cout << "  start Time: " << characters[i]["startTime"]  << std::endl;
-        std::cout << "    end Time: " << characters[i]["endTime"]    << std::endl;
-	
-	const Json::Value& teacherid = characters[i]["elements"];
-	for (int j =0; j < teacherid.size(); j++){
-		int type = teacherid[j]["type"].asInt();
-		if(type == 2){
-			int teacherId = teacherid[j]["id"].asInt();
-			const Json::Value& teacherNameJson = obj["result"]["data"]["elements"];	
-			for (int k = 0; k < teacherNameJson.size(); k++){
-				if(teacherNameJson[k]["id"] == teacherId){
-					std::cout << "teacher name: " << teacherNameJson[k]["displayname"] << std::endl;
+	std::string readBuffer;
+	curlRequest(&readBuffer, ClassID);
+
+	Json::Reader reader;
+	Json::Value obj;
+	reader.parse(readBuffer, obj); // reader can also read strings
+   	const Json::Value& characters = obj["result"]["data"]["elementPeriods"][std::to_string(ClassID)]; // array of characters
+   	for (int i = 0; i < characters.size(); i++){
+        	if(characters[i]["date"].asString() == getDate(0)){
+			if(lessonId != characters[i]["lessonId"].asInt()){
+				lessonText = characters[i]["lessonText"].asString();
+	    			startTime  = characters[i]["startTime"].asString();
+				endTime    = characters[i]["endTime"].asString();
+				lessonId   = characters[i]["lessonId"].asInt();
+				date	   = characters[i]["date"].asString();
+				teachers   = "";		
+				const Json::Value& teacherid = characters[i]["elements"];
+				for (int j = 0; j < teacherid.size(); j++){
+					int type = teacherid[j]["type"].asInt();
+					if(type == 2){
+						int teacherId = teacherid[j]["id"].asInt();
+						const Json::Value& teacherNameJson = obj["result"]["data"]["elements"];	
+						for (int k = 0; k < teacherNameJson.size(); k++){
+							if(teacherNameJson[k]["id"] == teacherId){
+								teachers += teacherNameJson[k]["longName"].asString() + "(" + teacherNameJson[k]["name"].asString() + "), ";
+							}	
+						}
+					}
 				}
-			}
-		}
-	}
-    }
+				teachers.pop_back();
+				teachers.pop_back();
+				int lessonId2 = lessonId;
+				int l = 0;
+				while(lessonId2 == lessonId){
+					endTime = characters[i+l]["endTime"].asString();
+					l++;
+					lessonId2 = characters[i+l]["lessonID"].asInt();
+				}
+				
+				std::ostringstream startTimeStream;
+				startTimeStream << std::setw(4) << std::setfill('0') << startTime;
+				startTime = startTimeStream.str();
+				startTime.insert(2,":");
+				
+				std::ostringstream endTimeStream;
+				endTimeStream << std::setw(4) << std::setfill('0') << endTime;
+				endTime = endTimeStream.str();
+				endTime.insert(2,":");
 
+				std::ostringstream lessonTimeStream;	
+				lessonTimeStream << startTime << " - " << endTime;
+				lessonTime = lessonTimeStream.str();
+				
+				std::cout << "Lesson: " << lessonText << std::endl;
+				std::cout << "time : " << lessonTime << std::endl;
+				std::cout << "date: " << date << std::endl;
+				std::cout << "docent: " << teachers << std::endl;
+				std::cout << std::endl;
+			}       		
+		}	
+	}
 }
 
 /**
@@ -108,10 +151,22 @@ std::string getCurlURL(int ID){
 * @note
 */
 std::string getDateField(){
-    time_t t = time(NULL);
-	tm* timePtr = localtime(&t);
     std::ostringstream streamDate;
-    streamDate << "date=" << (timePtr->tm_year + 1900) << std::setw(2) << std::setfill('0') << (timePtr->tm_mon + 1) << std::setw(2) << std::setfill('0') << timePtr->tm_mday << "&";
+    streamDate << "date=" << getDate(0) << "&";
+    std::string Date(streamDate.str());
+    return Date;
+}
+
+/**
+* @brief This function returns current date. given int days adds that amount in days to returned date.
+* @retuns String with date in following format: YYYYMMDD
+* @note
+*/
+std::string getDate(int days){
+    time_t t = time(NULL) + (86400 * days);
+    tm* timePtr = localtime(&t);
+    std::ostringstream streamDate;
+    streamDate << (timePtr->tm_year + 1900) << std::setw(2) << std::setfill('0') << (timePtr->tm_mon + 1) << std::setw(2) << std::setfill('0') << timePtr->tm_mday;
     std::string Date(streamDate.str());
     return Date;
 }
