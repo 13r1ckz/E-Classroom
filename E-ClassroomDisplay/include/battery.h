2e04ffe1560f7c9ef3e1df3dcbbb33a99b88f228
KinkyPeanut69 <balintharmse24@gmail.com>
2019-01-15 14:00:19 +0100

    Fixed conflict

--- E-ClassroomDisplay/include/battery.h
+++ E-ClassroomDisplay/include/battery.h
@@ -8,20 +8,12 @@
 #include <Arduino.h>
 
 /* Defines -------------------------------------------------------------------*/
-<<<<<<< HEAD
 #define VOLT_REDUCTION 11 // R2/(R1+R2 ) R1 = 10k ohm, R2 = 1k ohm
 #define ESP_VOLTAGE 3.2
 #define MAX_ANALOG_VALUE 1024
 #define OFFSET 0.30
 #define BATTER_MIN_VOLT 3.5
-=======
-#define voltReduction 11 // 1/(R2/(R1+R2 ) R1 = 10k ohm, R2 = 1k ohm
-#define espVoltage 3.2 //the max input voltage for nodemcu 1.0
-#define maxAnalogValue 1024 //the max analog value of the esp8266
-#define offset 0.30 //offset for getting a accurate measuring 
-#define batteryMinVolt 3.2 // lowest voltage that the battery can give 
-#define numberOfMeasurings 10 // how much measurings
->>>>>>> ESP8266-Display
+#define NUMBER_OF_MEASUREMENTS 10
 /* Classes -------------------------------------------------------------------*/
 class Battery{
 public:
